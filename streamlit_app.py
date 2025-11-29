24a4b23faf4c945fccd8b4d6930a608341820624
selliermatthias15-lgtm <sellier.matthias15@gmail.com>
2025-11-29 13:43:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -207,8 +207,8 @@ with col1:
 
         # Générer des couleurs dynamiques pour n_layers couches
         from matplotlib.cm import get_cmap
-        cmap_s = get_cmap('Greens')   # Schlumberger
-        cmap_w = get_cmap('plasma')    # Wenner
+        cmap_s = get_cmap('plasma')   # Schlumberger
+        cmap_w = get_cmap('Greens')    # Wenner
 
         # Découper AB2 en segments correspondant aux couches
         segments = np.array_split(np.arange(len(AB2)), n_layers)
