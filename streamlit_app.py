b9a7399bcf8d2583aa4d978221c313adaf7824ea
selliermatthias15-lgtm <sellier.matthias15@gmail.com>
2025-11-29 13:13:11 +0100

    Change colormap for Schlumberger and Wenner plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,8 +205,8 @@ with col1:
 
         # Générer des couleurs pour chaque courbe selon le nombre de couches
         from matplotlib.cm import get_cmap
-        cmap_s = get_cmap('tab10', n_layers)  # Schlumberger
-        cmap_w = get_cmap('tab20', n_layers)   # Wenner
+        cmap_s = get_cmap('viridis', n_layers)  # Schlumberger
+        cmap_w = get_cmap('magma', n_layers)   # Wenner
 
         # Découper AB2 en segments correspondant aux couches
         segments = np.array_split(np.arange(len(AB2)), n_layers)
